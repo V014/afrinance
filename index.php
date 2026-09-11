40942d9f5aa1b0bc55a02c84b6fe5a50e37e0a76
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2026-09-11 11:24:47 +0200

    testing extension changes

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes"">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/all.min.css" />
-    <script src="js/htmx.org@2.0.0.min.js"></script>
+    <script src="js/htmx.org@2.0.0.js"></script>
     <title>Afrinance</title>
 </head>
 <body>
